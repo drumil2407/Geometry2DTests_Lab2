16369c4c06244b310b447236799a3bf43628010b
drumil2407 <78135386+drumil2407@users.noreply.github.com>
2021-09-30 11:06:09 -0400

    Lab2 Particle and phonon

---
+++ Lab2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Lab2.Geometry2D;
+using Lab2.Particles;
+using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Vector p1 = new Vector();
+            p1.Set(1, 1);
+
+            Phonon p= new Phonon(1);
+
+            System.Console.WriteLine(p);
+
+            Point point = p.Position;
+
+            point.SetCoords(100,100);
+
+            System.Console.WriteLine(p);
+
+            Phonon p2 = new Phonon(p); // making a copy of point p and store in p2
+            Console.WriteLine(p2);
+
+            Phonon p3 = new Phonon(0);
+            p3.Drift(1);
+            Console.WriteLine(p3);
+
+        }
+    }
+}
---
+++ PhononTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhononTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DriftTest()
+        {
+            
+
+        }
+    }
+}
